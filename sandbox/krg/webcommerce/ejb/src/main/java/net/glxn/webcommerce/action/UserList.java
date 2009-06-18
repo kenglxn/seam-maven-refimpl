dd606a7e3de556abe7dde6eb0d275c678bee9395
ken@glxn.net <ken@glxn.net@1023f760-3e6f-11de-b83a-779c162a6e82>
2009-06-18 21:35:54 +0000

    added webcommerce project

---
+++ sandbox/krg/webcommerce/ejb/src/main/java/net/glxn/webcommerce/action/UserList.java
@@ -0,0 +1,14 @@
+package net.glxn.webcommerce.action;
+
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.framework.EntityQuery;
+import net.glxn.webcommerce.model.User;
+
+@Name("userList")
+public class UserList extends EntityQuery<User>
+{
+    public UserList()
+    {
+        setEjbql("select user from User user");
+    }
+}
