ed3bb75c2c3a28dda059f8a78f6ca1ea458d8bde
mr.leif.olsen <mr.leif.olsen@1023f760-3e6f-11de-b83a-779c162a6e82>
2009-07-01 20:57:29 +0000

    seaminaction/stages/projects-ch06, modifications to original code:
    action/src/main/java/org/open18/action/*List.java
    - Removed getRestrictions() method in all *List.java files. 
    - Added constructor with setRestrictionExpressionStrings(Arrays.asList(RESTRICTIONS)) method call 
    - Cause for this is outlined in:
      http://code.google.com/p/seam-maven-refimpl/wiki/SeamInActionCh04/chapter-developments.txt
    
    action/src/main/java/org/open18/action/TeeList.java, 
    war/src/webapp//TeeList.xhtml
    - Fixed count query problem as described here:
      http://www.seamframework.org/Community/OrghibernatehqlastQuerySyntaxExceptionExpectingCLOSEFoundNull
      
    Pagination
    - Fixed search/pagination problem as described here:
      http://seamframework.org/Community/PaginationAndEntityQuery

--- samples/seaminaction/stages/projects-ch06/action/src/main/java/org/open18/action/FacilityList.java
+++ samples/seaminaction/stages/projects-ch06/action/src/main/java/org/open18/action/FacilityList.java
@@ -10,7 +10,7 @@ import org.jboss.seam.contexts.FacesLifecycle;
 import org.jboss.seam.log.Log;
 
 @Name("facilityList")
-public class FacilityList extends EntityQuery {
+public class FacilityList extends EntityQuery<Facility>  {
 
 	@Logger
 	private Log log;
@@ -30,6 +30,10 @@ public class FacilityList extends EntityQuery {
 
 	private Facility facility = new Facility();
 
+	public FacilityList() {
+		setRestrictionExpressionStrings(Arrays.asList(RESTRICTIONS));
+	}
+	
 	@Override
 	public String getEjbql() {
 		return "select facility from Facility facility";
@@ -52,9 +56,29 @@ public class FacilityList extends EntityQuery {
 		return facility;
 	}
 
+/*
+ * LOO-20090630, NOTE: This does not work. 
+ * Must use setRestrictionExpressionStrings(Arrays.asList(RESTRICTIONS)); in constructor
+ *
 	@Override
 	public List<String> getRestrictions() {
 		return Arrays.asList(RESTRICTIONS);
+	}
+ */
+ 
+/*
+ * LOO-20090630. fixed pagination propblem as described here:
+ * http://seamframework.org/Community/PaginationAndEntityQuery
+ */ 
+	@Override
+	public List<Facility> getResultList() {
+		List<Facility> result = super.getResultList();
+		if (getFirstResult() != null && getFirstResult() > 0 
+				&& (result == null ||  result.size() == 0)) {
+			setFirstResult(0);
+			refresh();
+		}
+		return super.getResultList();
 	}
 
 	public void preloadFacilities() {
--- samples/seaminaction/stages/projects-ch06/action/src/main/java/org/open18/action/TeeSetList.java
+++ samples/seaminaction/stages/projects-ch06/action/src/main/java/org/open18/action/TeeSetList.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Arrays;
 
 @Name("teeSetList")
-public class TeeSetList extends EntityQuery {
+public class TeeSetList extends EntityQuery<TeeSet> {
 
 	private static final String[] RESTRICTIONS = {
 			"lower(teeSet.color) like concat(lower(#{teeSetList.teeSet.color}),'%')",
@@ -15,6 +15,10 @@ public class TeeSetList extends EntityQuery {
 
 	private TeeSet teeSet = new TeeSet();
 
+	public TeeSetList() {
+		setRestrictionExpressionStrings(Arrays.asList(RESTRICTIONS));
+	}
+	
 	@Override
 	public String getEjbql() {
 		return "select teeSet from TeeSet teeSet";
@@ -29,9 +33,28 @@ public class TeeSetList extends EntityQuery {
 		return teeSet;
 	}
 
+/*
+ * LOO-20090627, NOTE: This does not work. 
+ * Must use setRestrictionExpressionStrings(Arrays.asList(RESTRICTIONS)); in constructor
+ *
 	@Override
 	public List<String> getRestrictions() {
 		return Arrays.asList(RESTRICTIONS);
 	}
-
+ */
+ 
+/*
+ * LOO-20090630. fixed pagination propblem as described here:
+ * http://seamframework.org/Community/PaginationAndEntityQuery
+ */ 
+	@Override
+	public List<TeeSet> getResultList() {
+		List<TeeSet> result = super.getResultList();
+		if (getFirstResult() != null && getFirstResult() > 0 
+				&& (result == null ||  result.size() == 0)) {
+			setFirstResult(0);
+			refresh();
+		}
+		return super.getResultList();
+	}
 }
