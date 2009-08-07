4784e16e7a3248bdc6ceebc0eec6a7b1f4d518bb
ken@glxn.net <ken@glxn.net@1023f760-3e6f-11de-b83a-779c162a6e82>
2009-08-07 15:21:03 +0000

    initial check-in for jpa-console

---
+++ sandbox/ejb/src/main/java/org/jboss/seam/example/action/Authenticator.java
@@ -0,0 +1,33 @@
+package org.jboss.seam.example.action;
+
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.log.Log;
+import org.jboss.seam.security.Credentials;
+import org.jboss.seam.security.Identity;
+
+@Name("authenticator")
+public class Authenticator
+{
+    @Logger private Log log;
+
+    @In Identity identity;
+    @In Credentials credentials;
+
+    public boolean authenticate()
+    {
+        log.info("authenticating {0}", credentials.getUsername());
+        //write your authentication logic here,
+        //return true if the authentication was
+        //successful, false otherwise. mmm
+        if ("admin".equals(credentials.getUsername()))
+        {
+            identity.addRole("admin");
+            //System.out.println("LOGGED IN!");
+            return true;
+        }
+        return false;
+    }
+
+}
