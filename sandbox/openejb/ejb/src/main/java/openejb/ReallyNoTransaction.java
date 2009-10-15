1edd2654483afaebab76c25126eb85ae8f97c48a
ken@glxn.net <ken@glxn.net@1023f760-3e6f-11de-b83a-779c162a6e82>
2009-10-15 07:27:46 +0000

    openejb sandbox project, used to test out openejb and get testing up n running

---
+++ sandbox/openejb/ejb/src/main/java/openejb/ReallyNoTransaction.java
@@ -0,0 +1,55 @@
+package openejb2;
+
+import org.jboss.seam.annotations.intercept.BypassInterceptors;
+import org.jboss.seam.transaction.NoTransaction;
+
+import javax.transaction.SystemException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.Status;
+import javax.transaction.Synchronization;
+
+@BypassInterceptors
+public class ReallyNoTransaction
+        extends NoTransaction {
+
+    @Override
+    public void begin () throws NotSupportedException, SystemException {
+
+    }
+
+    @Override
+    public void commit () throws RollbackException, HeuristicMixedException,
+            HeuristicRollbackException, SecurityException,
+                                 IllegalStateException, SystemException {
+
+    }
+
+    @Override
+    public int getStatus () throws SystemException {
+        return Status.STATUS_NO_TRANSACTION;
+    }
+
+    @Override
+    public void rollback () throws IllegalStateException, SecurityException, SystemException {
+
+    }
+
+    @Override
+    public void setRollbackOnly () throws IllegalStateException, SystemException {
+
+    }
+
+    @Override
+    public void setTransactionTimeout (final int pTimeout) throws SystemException {
+
+    }
+
+    @Override
+    public void registerSynchronization (final Synchronization sync) {
+
+    }
+
+}
