2de90052b5fa358902dd1e050f973b8362ef63f2
mr.leif.olsen <mr.leif.olsen@1023f760-3e6f-11de-b83a-779c162a6e82>
2009-11-16 12:21:54 +0000

    added work copy of seam-refimpl

---
+++ sandbox/loo/seam-refimpl/ejb/src/main/java/org/jboss/seam/example/model/User.java
@@ -0,0 +1,82 @@
+package org.jboss.seam.example.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Version;
+import org.hibernate.validator.Length;
+import org.hibernate.validator.NotNull;
+
+@Entity
+@Table(name="users")
+public class User implements Serializable
+{
+	private static final long serialVersionUID = 1881413500711441951L;
+	
+    private Long id;
+    private Integer version;
+	private String username;
+    private String password;
+    private String name;
+
+    public User(String name, String password, String username) {
+        this.name = name;
+        this.password = password;
+        this.username = username;
+    }
+
+    public User() {
+    }
+
+    @Id @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Version
+    public Integer getVersion() {
+        return version;
+    }
+
+    private void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    @NotNull @Length(min=5, max=15)
+    public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@NotNull @Length(min=5, max=15)
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() 
+    {
+       return "User(" + username + ")";
+    }
+}
