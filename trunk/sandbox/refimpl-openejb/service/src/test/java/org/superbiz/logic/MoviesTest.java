6bebf8fb93f66ae62f4c32e997764240a2356e22
mr.leif.olsen <mr.leif.olsen@1023f760-3e6f-11de-b83a-779c162a6e82>
2009-11-22 01:02:13 +0000

    Work in progress: OpenEJB test environment for Seam

--- trunk/sandbox/refimpl-openejb/service/src/test/java/org/superbiz/logic/MoviesTest.java
+++ trunk/sandbox/refimpl-openejb/service/src/test/java/org/superbiz/logic/MoviesTest.java
@@ -21,52 +21,53 @@ public class MoviesTest {
 
 	protected static Context context;
 
-	public MoviesTest() {
-	}
-
-	@BeforeClass
-	public void setUp() throws Exception {
-		Properties p = new Properties();
-		
-		p.put("openejb.embedded.initialcontext.close", "destroy"); // http://blog.jonasbandi.net/2009/06/restarting-embedded-openejb-container.html
-		p.put("log4j.category.org.superbiz", "warn"); 
-		p.put("log4j.category.org.superbiz.logic.MoviesTest", "debug"); 
-		
-		context = OpenEjbBootStrap.bootstrap(p);
-	}
-
-	@AfterClass
-	public void tearDown() throws Exception {
-		context.close();
-	}
-
-	private Movies createService() throws Exception {
-		try {
-			Object obj = context.lookup("moviesLocal");
-			Assert.assertNotNull(obj, "context.lookup(moviesLocal): returned null");
-			Assert.assertTrue(obj instanceof Movies, "context.lookup(moviesLocal): incorrect type");
-			return (Movies) obj;
-		}
-		catch (NamingException e) {
-			log.error(e);
-			throw(e);
-		}
-	}
-	
-	@Test
-	public void test() throws Exception {
-		Movies movies = createService();
-		movies.addMovie(new Movie("Quentin Tarantino", "Reservoir Dogs", 1992));
-		movies.addMovie(new Movie("Joel Coen", "Fargo", 1996));
-		movies.addMovie(new Movie("Joel Coen", "The Big Lebowski", 1998));
-
-		List<Movie> list = movies.getMovies();
-		Assert.assertEquals(3, list.size(), "List.size()");
-
-		for (Movie movie : list) {
-			movies.deleteMovie(movie);
-		}
-
-		Assert.assertEquals(0, movies.getMovies().size(), "Movies.getMovies()");
-	}
+//	public MoviesTest() {
+//	}
+//
+//	@BeforeClass
+//	public void setUp() throws Exception {
+//		Properties p = new Properties();
+//		
+//		p.put("openejb.embedded.initialcontext.close", "destroy"); // http://blog.jonasbandi.net/2009/06/restarting-embedded-openejb-container.html
+//		p.put("openejb.jndiname.format", "{deploymentId}/{interfaceType.annotationName}");
+//		p.put("log4j.category.org.superbiz", "warn"); 
+//		p.put("log4j.category.org.superbiz.logic.MoviesTest", "debug"); 
+//		
+//		context = OpenEjbBootStrap.bootstrap(p);
+//	}
+//
+//	@AfterClass
+//	public void tearDown() throws Exception {
+//		context.close();
+//	}
+//
+//	private Movies createService() throws Exception {
+//		try {
+//			Object obj = context.lookup("movies/Local");
+//			Assert.assertNotNull(obj, "context.lookup(moviesLocal): returned null");
+//			Assert.assertTrue(obj instanceof Movies, "context.lookup(moviesLocal): incorrect type");
+//			return (Movies) obj;
+//		}
+//		catch (NamingException e) {
+//			log.error(e);
+//			throw(e);
+//		}
+//	}
+//	
+//	@Test
+//	public void test() throws Exception {
+//		Movies movies = createService();
+//		movies.addMovie(new Movie("Quentin Tarantino", "Reservoir Dogs", 1992));
+//		movies.addMovie(new Movie("Joel Coen", "Fargo", 1996));
+//		movies.addMovie(new Movie("Joel Coen", "The Big Lebowski", 1998));
+//
+//		List<Movie> list = movies.getMovies();
+//		Assert.assertEquals(3, list.size(), "List.size()");
+//
+//		for (Movie movie : list) {
+//			movies.deleteMovie(movie);
+//		}
+//
+//		Assert.assertEquals(0, movies.getMovies().size(), "Movies.getMovies()");
+//	}
 }
