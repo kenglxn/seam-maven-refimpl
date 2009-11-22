448b1f413bf59cc258db197a5b37286b65e4a121
mr.leif.olsen <mr.leif.olsen@1023f760-3e6f-11de-b83a-779c162a6e82>
2009-11-22 19:36:31 +0000

    Work in progress: OpenEJB test environment for Seam

--- trunk/sandbox/refimpl-openejb/openejb-utils/src/main/java/no/knowit/openejb/BootStrapOpenEjb.java
+++ trunk/sandbox/refimpl-openejb/openejb-utils/src/main/java/no/knowit/openejb/BootStrapOpenEjb.java
@@ -7,8 +7,6 @@ import javax.naming.InitialContext;
 
 public class BootStrapOpenEjb {
 
-	//protected static Logger log = Logger.getLogger(OpenEjbBootStrap.class);
-
 	public static Context bootstrap() throws Exception {
 		return bootstrap(null);
 	}
@@ -17,10 +15,18 @@ public class BootStrapOpenEjb {
 		try {
 			// Check that OpenEJB is available
 			Class.forName("org.apache.openejb.OpenEJB");
-			
-			// Initial properties are specified in src/main/resources-openejb/jndi.properties
-			// Properties defined in properties param will override initial properties  
-			return new InitialContext(properties);
+
+			// Set some default values
+			Properties p = new Properties();
+			p.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.client.LocalInitialContextFactory");
+			p.put("openejb.jndiname.format", "{deploymentId}/{interfaceType.annotationName}"); // Corresponds to JBoss JNDI lookup
+			p.put("openejb.deployments.classpath.ear", "true"); //for multi module projects
+
+			// Overrides default properties if key match
+			if(properties != null) {
+				p.putAll(properties);
+			}
+			return new InitialContext(p);
 		} 
 		catch (Exception e) {
 			// Since OpenEJB failed to start, we do not have a logger
