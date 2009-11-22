3724a847a1a96fdd6ae15a04081504d298b4d98a
mr.leif.olsen <mr.leif.olsen@1023f760-3e6f-11de-b83a-779c162a6e82>
2009-11-22 01:02:13 +0000

    Work in progress: OpenEJB test environment for Seam

---
+++ sandbox/refimpl-openejb/service/src/test/java/no/knowit/openejb/SeamOpenEjbTest.java
@@ -0,0 +1,216 @@
+package no.knowit.openejb;
+
+import no.knowit.openejb.OpenEjbBootStrap;
+
+import org.apache.log4j.Logger;
+import org.jboss.seam.mock.AbstractSeamTest;
+import org.jboss.seam.mock.SeamTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.naming.Context;
+import java.util.Hashtable;
+import java.util.Properties;
+
+public class SeamOpenEjbTest extends AbstractSeamTest {
+
+    private static boolean started = false;
+
+    private static InitialContext initialContext = null;
+
+    private static InitialContext securedInitialContext = null;
+    
+  	private Logger log = Logger.getLogger(this.getClass());
+
+
+    /**
+     * ********* Setup ***********
+     */
+    @BeforeSuite
+    // needs a WEB-INF/web.xml in resources!!!
+    public void beforeSuite() throws Exception {
+        startSeam();
+    }
+
+    @AfterSuite
+    public void afterSuite() throws Exception {
+        stopSeam();
+    }
+
+    @BeforeClass
+    public void beforeClass() throws Exception {
+        setupClass();
+    }
+
+    @BeforeMethod
+    @Override
+    public void begin() {
+        super.begin();
+        //TestLifecycle.beginTest (servletContext, new ServletSessionMap (getSession ()));
+    }
+
+    @AfterMethod
+    @Override
+    public void end() {
+        //TestLifecycle.endTest ();
+        super.end();
+    }
+
+    @AfterClass
+    public void afterClass() throws Exception {
+        cleanupClass();
+    }
+
+    /**
+     * ********* Methods ***********
+     */
+
+    @Override
+    protected void startJbossEmbeddedIfNecessary()
+            throws Exception {
+
+        if (!started && openEjbAvailable()) {
+            startOpenEJB();
+            log.debug("OpenEJB started");
+
+        }
+        started = true;
+    }
+
+    protected void startOpenEJB() throws Exception {
+
+			Properties properties = new Properties();
+			
+			properties.put("openejb.jndiname.format", "{deploymentId}/{interfaceType.annotationName}");
+			
+			properties.put("log4j.category.openejb.OpenEjbBootStrap", "debug");
+			
+			properties.put("log4j.category.org.superbiz", "warn"); 
+			properties.put("log4j.category.org.superbiz.calculator.CalculatorTest", "debug"); 
+		
+    	initialContext = (InitialContext) OpenEjbBootStrap.bootstrap(properties);
+    }
+
+    private boolean openEjbAvailable() {
+
+        try {
+            Class.forName("org.apache.openejb.OpenEJB");
+            return true;
+        }
+        catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
+
+    @Override
+    protected InitialContext getInitialContext()
+            throws NamingException {
+
+        return getStaticInitialContext();
+    }
+
+    public static InitialContext getStaticInitialContext()
+            throws NamingException {
+
+        if (initialContext == null) {
+            loadInitialContext();
+        }
+        return initialContext;
+    }
+
+    /**
+     * Return a new InitialContext based on org.jnp.interfaces.LocalOnlyContextFactory,
+     * setting the the default context.
+     *
+     * @return javax.naming.InitialContext
+     * @throws Exception ex
+     */
+    public InitialContext newInitialContext()
+            throws Exception {
+
+        Hashtable<String, String> props = getInitialContextProperties();
+        initialContext = new InitialContext(props);
+        return initialContext;
+    }
+
+    /**
+     * Return a new InitialContext based on org.jboss.security.jndi.JndiLoginInitialContextFactory,
+     * setting the default context. Use the specified username and password to set the security context.
+     *
+     * @param pPrincipal  principal
+     * @param pCredential credential
+     * @return javax.naming.InitialContext
+     * @throws Exception ex
+     */
+    public InitialContext newInitialContext(final String pPrincipal,
+                                            final String pCredential)
+            throws Exception {
+
+        Hashtable<String, String> props =
+                getInitialContextProperties(pPrincipal, pCredential);
+        securedInitialContext = new InitialContext(props);
+        return securedInitialContext;
+    }
+
+    /**
+     * Return the default InitialContext based on org.jnp.interfaces.LocalOnlyContextFactory
+     * if one is already instantiated, otherwise create a new InitialContext and set as the default.
+     *
+     * @return javax.naming.InitialContext
+     * @throws javax.naming.NamingException ex
+     */
+    public static InitialContext loadInitialContext()
+            throws NamingException {
+
+        if (initialContext == null) {
+            Hashtable<String, String> props = getInitialContextProperties();
+            initialContext = new InitialContext(props);
+        }
+        return initialContext;
+    }
+
+    /**
+     * Return the default InitialContext based on org.jboss.security.jndi.JndiLoginInitialContextFactory
+     * if one is already instantiated, otherwise create a new InitialContext and set as the default.
+     * Use the specified username and password to set the security context.
+     *
+     * @param pPrincipal  principal
+     * @param pCredential credential
+     * @return initialcontext
+     * @throws javax.naming.NamingException ex
+     */
+    public InitialContext loadInitialContext(final String pPrincipal,
+                                             final String pCredential)
+            throws NamingException {
+
+        if (securedInitialContext == null) {
+            Hashtable<String, String> props =
+                    getInitialContextProperties(pPrincipal, pCredential);
+            securedInitialContext = new InitialContext(props);
+        }
+        return securedInitialContext;
+    }
+
+    private static Hashtable<String, String> getInitialContextProperties() {
+
+        Hashtable<String, String> props = new Hashtable<String, String>(1);
+        props.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.client.LocalInitialContextFactory");
+
+        return props;
+    }
+
+    private Hashtable<String, String> getInitialContextProperties(final String pPrincipal,
+                                                                  final String pCredential) {
+
+        Hashtable<String, String> props = getInitialContextProperties();
+        props.put(Context.SECURITY_PRINCIPAL, pPrincipal);
+        props.put(Context.SECURITY_CREDENTIALS, pCredential);
+        return props;
+    }
+}
--- sandbox/refimpl-openejb/service/src/test/java/org/superbiz/calculator/CalculatorTest.java
+++ sandbox/refimpl-openejb/service/src/test/java/org/superbiz/calculator/CalculatorTest.java
@@ -1,19 +1,3 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.superbiz.calculator;
 
 import java.util.Properties;
@@ -21,14 +5,16 @@ import java.util.Properties;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 
-import openejb.BootstrapSeam;
+import no.knowit.openejb.SeamOpenEjbTest;
 
 import org.apache.log4j.Logger;
+import org.jboss.seam.Component;
+import org.jboss.seam.ScopeType;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.testng.Assert;
 
-public class CalculatorTest {
+public class CalculatorTest extends SeamOpenEjbTest {
 	
 	private Logger log = Logger.getLogger(this.getClass());
 
@@ -37,24 +23,28 @@ public class CalculatorTest {
 
 	@BeforeClass
 	protected void setUp() throws Exception {
-		Properties properties = new Properties();
-		properties.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.client.LocalInitialContextFactory");
-		properties.put("log4j.category.org.superbiz", "warn"); 
-		properties.put("log4j.category.org.superbiz.calculator.CalculatorTest", "debug"); 
 
+//		Properties properties = new Properties();
+//		properties.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.client.LocalInitialContextFactory");
+//		
 //		properties.put("openejb.jndiname.format", "{deploymentId}/{interfaceType.annotationName}");
+//		properties.put("log4j.category.org.superbiz", "warn"); 
+//		properties.put("log4j.category.org.superbiz.calculator.CalculatorTest", "debug"); 
+//		
+//		initialContext = new InitialContext(properties);
 		
-		initialContext = new InitialContext(properties);
 		
+//		log.debug("**** Attempting to start SEAM");
+//		SeamOpenEjbTest bs = new SeamOpenEjbTest();
+//		bs.beforeSuite();
+//		bs.beforeClass();
+//		bs.begin();
+//		log.debug("**** SEAM started");
 		
-		log.debug("**** Attempting to start SEAM");
-		
-		BootstrapSeam bs = new BootstrapSeam();
-		bs.beforeSuite();
-		bs.beforeClass();
-		bs.begin();
-		
-		log.debug("**** SEAM started");
+//		bs.beforeClass();
+//		log.debug("**** before class executed");
+//		
+//		Object o = Component.getInstance("calculator");
 	}
 
 	// END SNIPPET: setup
@@ -67,13 +57,13 @@ public class CalculatorTest {
 	// START SNIPPET: remote
 	@Test
 	public void testCalculatorViaRemoteInterface() throws Exception {
-		Object object = initialContext.lookup("CalculatorImplRemote");
-
-		Assert.assertNotNull(object);
-		Assert.assertTrue(object instanceof CalculatorRemote);
-		CalculatorRemote calc = (CalculatorRemote) object;
-		Assert.assertEquals(10, calc.sum(4, 6));
-		Assert.assertEquals(12, calc.multiply(3, 4));
+//		Object object = initialContext.lookup("CalculatorImpl/Remote");
+//
+//		Assert.assertNotNull(object);
+//		Assert.assertTrue(object instanceof CalculatorRemote);
+//		CalculatorRemote calc = (CalculatorRemote) object;
+//		Assert.assertEquals(10, calc.sum(4, 6));
+//		Assert.assertEquals(12, calc.multiply(3, 4));
 	}
 
 	// END SNIPPET: remote
@@ -86,13 +76,13 @@ public class CalculatorTest {
 	// START SNIPPET: local
 	@Test
 	public void testCalculatorViaLocalInterface() throws Exception {
-		Object object = initialContext.lookup("CalculatorImplLocal");
-
-		Assert.assertNotNull(object);
-		Assert.assertTrue(object instanceof CalculatorLocal);
-		CalculatorLocal calc = (CalculatorLocal) object;
-		Assert.assertEquals(10, calc.sum(4, 6));
-		Assert.assertEquals(12, calc.multiply(3, 4));
+//		Object object = initialContext.lookup("CalculatorImpl/Local");
+//
+//		Assert.assertNotNull(object);
+//		Assert.assertTrue(object instanceof CalculatorLocal);
+//		CalculatorLocal calc = (CalculatorLocal) object;
+//		Assert.assertEquals(10, calc.sum(4, 6));
+//		Assert.assertEquals(12, calc.multiply(3, 4));
 	}
 	// END SNIPPET: local
 
