dfc190f421cc74134ee7b75c79e025864db76236
mr.leif.olsen <mr.leif.olsen@1023f760-3e6f-11de-b83a-779c162a6e82>
2009-11-24 22:44:02 +0000

    Work in progress: OpenEJB test environment for Seam

--- sandbox/refimpl-openejb/service/src/main/java/org/superbiz/openejb/SeamManagedPersistenceUnit.java
+++
@@ -1,9 +0,0 @@
-package org.superbiz.openejb;
-
-import javax.ejb.Local;
-import javax.persistence.EntityManagerFactory;
-
-@Local
-public interface SeamManagedPersistenceUnit {
-	EntityManagerFactory getEntityMangagerFactory();
-}
