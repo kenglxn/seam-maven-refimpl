0c62f230a8e79346f16ed8bb9c801b3bea58e9e1
mr.leif.olsen <mr.leif.olsen@1023f760-3e6f-11de-b83a-779c162a6e82>
2009-11-25 16:33:02 +0000

    Work in progress: OpenEJB test environment for Seam

--- sandbox/refimpl-openejb/seam-openejb-utils-test/src/test/java/no/knowit/openejb/BootstrapOpenEjbTest.java
+++ sandbox/refimpl-openejb/seam-openejb-utils-test/src/test/java/no/knowit/openejb/BootstrapOpenEjbTest.java
@@ -19,7 +19,7 @@ public class BootstrapOpenEjbTest {
 	public void setUp() throws Exception {
 		Properties p = BootStrapOpenEjb.getDefaultProperties(null);
 
-		// Override default property value
+		// Override default property values
 		p.put("openejb.deployments.classpath.ear", "false");
 		
 		// Database connection (same as *-ds.xml in JBoss)
@@ -28,13 +28,12 @@ public class BootstrapOpenEjbTest {
 		p.put("openejbTestDatabase.JdbcUrl ", "jdbc:hsqldb:mem:moviedb");
 
 		// override properties on "openejb-test-unit" persistence unit (src/main/resources/META-INF/persistence.xml)
-		p.put("openejb-test-unit.hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
-		
+		//p.put("openejb-test-unit.hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
 		
 		// Change some logging, INFO|DEBUG|WARN|ERROR|FATAL
+		p.put("log4j.category.no.knowit.openejb.BootstrapOpenEjbTest", "debug");
 		p.put("log4j.category.org.jboss.seam.Component", "DEBUG");
 		p.put("log4j.category.org.jboss.seam.mock", "DEBUG");
-		p.put("log4j.category.no.knowit.openejb", "debug");
 	}
 
 	/**
@@ -43,10 +42,12 @@ public class BootstrapOpenEjbTest {
 	 */
 	@Test
 	public void shallBootstrapOpenEjb() throws Exception {
+		
+		
 		initialContext = BootStrapOpenEjb.bootstrap(null);
 		Assert.assertNotNull(initialContext);
 	}
-
+	
 	/**
 	 * 
 	 * @throws Exception
@@ -67,5 +68,4 @@ public class BootstrapOpenEjbTest {
 		initialContext = BootStrapOpenEjb.shutdown();
 		Assert.assertFalse(OpenEJB.isInitialized());
 	}
-	
 }
