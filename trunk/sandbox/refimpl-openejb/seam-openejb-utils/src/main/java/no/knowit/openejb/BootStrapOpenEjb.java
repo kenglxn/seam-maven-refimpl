275c90d486fc9539d788f10a907a0a5e5bb06ae4
mr.leif.olsen <mr.leif.olsen@1023f760-3e6f-11de-b83a-779c162a6e82>
2009-11-25 08:16:53 +0000

    Work in progress: OpenEJB test environment for Seam

--- trunk/sandbox/refimpl-openejb/seam-openejb-utils/src/main/java/no/knowit/openejb/BootStrapOpenEjb.java
+++ trunk/sandbox/refimpl-openejb/seam-openejb-utils/src/main/java/no/knowit/openejb/BootStrapOpenEjb.java
@@ -5,8 +5,13 @@ import java.util.Properties;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 
+import org.apache.openejb.OpenEJB;
+import org.apache.openejb.assembler.classic.AppInfo;
+import org.apache.openejb.assembler.classic.Assembler;
+import org.apache.openejb.loader.SystemInstance;
+
 public class BootStrapOpenEjb {
-	
+
 	private static InitialContext initialContext = null;
 
 	public static InitialContext bootstrap() throws Exception {
@@ -20,37 +25,38 @@ public class BootStrapOpenEjb {
 	 * @throws Exception
 	 */
 	public static InitialContext bootstrap(final Properties properties) throws Exception {
-		
+
 		try {
 			// Is OpenEJB is available
 			Class.forName("org.apache.openejb.OpenEJB");
 
-			if(initialContext != null) initialContext.close();
-			
+			if (initialContext != null) {
+				initialContext.close();
+			}
+
 			// Set some default values
 			Properties p = new Properties();
-			
+
 			// Set the initial context factory
 			p.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.client.LocalInitialContextFactory");
-			
+
 			// Corresponds to JBoss JNDI lookup
 			p.put("openejb.jndiname.format", "{deploymentId}/{interfaceType.annotationName}");
-			
+
 			// For multi module projects
 			p.put("openejb.deployments.classpath.ear", "true");
-			
+
 			// see: http://openejb.apache.org/3.0/alternate-descriptors.html
 			p.put("openejb.altdd.prefix", "openejb");
 
 			// Change some logging, INFO|DEBUG|WARN|ERROR|FATAL
-			p.put("log4j.category.no.knowit.seam", "warn");
+			p.put("log4j.category.no.knowit", "warn");
 
-			
 			// Overrides default properties in p if key match
-			if(properties != null) {
+			if (properties != null) {
 				p.putAll(properties);
 			}
-			
+
 			initialContext = new InitialContext(p);
 			return initialContext;
 		} 
@@ -60,21 +66,56 @@ public class BootStrapOpenEjb {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	/**
 	 * 
 	 */
 	public static InitialContext closeInitialContext() {
-		if(initialContext != null) {
+		if (initialContext != null) {
 			try {
 				initialContext.close();
 				initialContext = null;
-			} catch (Exception e) {
+			} 
+			catch (Exception e) {
 				System.out.println("\n*******\nClosing OpenEJB context failed: " + e + "\n*******");
 				throw new RuntimeException(e);
 			}
 		}
 		return null;
+	}
+
+	/**
+	 * Perform a clean shutdown of this embedded instance
+	 */
+	public static void shutdown() {
+		if (OpenEJB.isInitialized()) {
+			Assembler assembler = SystemInstance.get().getComponent(Assembler.class);
+			try {
+				for (AppInfo appInfo : assembler.getDeployedApplications()) {
+						assembler.destroyApplication(appInfo.jarPath);
+				}
+			} 
+			catch (Exception e) {
+				System.out.println("\n*******\nClosing OpenEJB context failed: " + e + "\n*******");
+				throw new RuntimeException(e);
+			}
+			finally {
+				OpenEJB.destroy();
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @return <code>true</code> if OpenEJB is available in classpath
+	 */
+	public static Boolean isopenEjbAvailable() {
+		try {
+			Class.forName("org.apache.openejb.OpenEJB");
+		} catch (ClassNotFoundException e) {
+			return false;
+		}
+		return true;
 	}
 
 }
