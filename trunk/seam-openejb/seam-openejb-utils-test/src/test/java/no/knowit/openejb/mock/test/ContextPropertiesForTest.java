bdd2c49cc1be81d8aef2fae9448a4cc856000d88
mr.leif.olsen <mr.leif.olsen@1023f760-3e6f-11de-b83a-779c162a6e82>
2009-12-09 00:23:57 +0000

    OpenEJB test environment for Seam: Seam Managed Transactions working

--- trunk/seam-openejb/seam-openejb-utils-test/src/test/java/no/knowit/openejb/mock/test/ContextPropertiesForTest.java
+++ trunk/seam-openejb/seam-openejb-utils-test/src/test/java/no/knowit/openejb/mock/test/ContextPropertiesForTest.java
@@ -15,18 +15,27 @@ public class ContextPropertiesForTest {
 
 		// Override default property values
 		p.put("openejb.deployments.classpath.ear", "false");
+		p.put("openejb.altdd.prefix", "openejb");		
 		
 		// Database connection (same as *-ds.xml in JBoss)
-		p.put("openejbTestDatabase = new://Resource?type", "DataSource");
-		p.put("openejbTestDatabase.JdbcDriver ", "org.hsqldb.jdbcDriver");
-		p.put("openejbTestDatabase.JdbcUrl ", "jdbc:hsqldb:mem:moviedb");
+		p.put("openejbDatabase = new://Resource?type", "DataSource");
+		p.put("openejbDatabase.JdbcDriver ",           "org.hsqldb.jdbcDriver");
+		p.put("openejbDatabase.JdbcUrl ",              "jdbc:hsqldb:mem:openejb_db");
+		
+    p.put("openejbDatabaseUnmanaged",              "new://Resource?type=DataSource");
+    p.put("openejbDatabaseUnmanaged.JdbcDriver",   "org.hsqldb.jdbcDriver");
+    p.put("openejbDatabaseUnmanaged.JdbcUrl",      "jdbc:hsqldb:mem:openejb_db");
+    p.put("openejbDatabaseUnmanaged.JtaManaged",   "false");
 
-		// override properties on "openejb-test-unit" persistence unit (src/main/resources/META-INF/persistence.xml)
+
+		// override properties on "openejb-unit" persistence unit (src/main/resources/META-INF/persistence.xml)
 		//p.put("openejb-test-unit.hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
 		
 		// Change some logging, INFO|DEBUG|WARN|ERROR|FATAL
 		p.put("log4j.category.org.jboss.seam.Component", "DEBUG");
+		p.put("log4j.category.org.jboss.seam.transaction", "DEBUG");
 		p.put("log4j.category.org.jboss.seam.mock", "DEBUG");
+		p.put("log4j.category.no.knowit.seam.openejb.mock", "DEBUG");
 
 		if (properties != null) {
 			p.putAll(properties);
