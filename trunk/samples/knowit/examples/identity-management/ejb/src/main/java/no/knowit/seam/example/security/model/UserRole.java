2bacfded8488e5128aa3451c25fc8525c11cda1b
mr.leif.olsen <mr.leif.olsen@1023f760-3e6f-11de-b83a-779c162a6e82>
2010-03-25 00:37:06 +0000

    Added identity-management project to samples/knowit/examples see: http://blog.hibernate.org/10904.lace

---
+++ trunk/samples/knowit/examples/identity-management/ejb/src/main/java/no/knowit/seam/example/security/model/UserRole.java
@@ -0,0 +1,89 @@
+package no.knowit.seam.example.security.model;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Version;
+
+import org.hibernate.validator.NotNull;
+import org.jboss.seam.annotations.security.management.RoleConditional;
+import org.jboss.seam.annotations.security.management.RoleGroups;
+import org.jboss.seam.annotations.security.management.RoleName;
+
+@Entity
+public class UserRole implements Serializable {
+	private static final long serialVersionUID = 9177366120789064801L;
+
+	private Long id;
+  private Long version;
+	private String name;
+	private boolean conditional;
+	private Set<UserRole> groups;
+
+	public UserRole() {
+	}
+	
+	@Id 
+	@GeneratedValue
+	@Column
+	public Long getId() {
+		return id;
+	}
+
+	// setter needed by JPA, but protected because it makes no sense for the application to assign an id.
+	protected void setId(Long id) {
+		this.id = id;
+	}
+	
+  @Version
+  @Column
+  public Long getVersion() {
+    return version;
+  }
+  
+  protected void setVersion(Long version) {
+		this.version = version;
+	}
+  
+	@RoleName
+	@NotNull
+  @Column(unique=true, nullable=false)
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	@RoleConditional
+	public boolean isConditional() {
+		return conditional;
+	}
+	public void setConditional(boolean conditional) {
+		this.conditional = conditional;
+	}
+	
+	@RoleGroups
+	@ManyToMany
+	@JoinTable(name = "UserRoleGroup", 
+			joinColumns = @JoinColumn(name = "roleId"), 
+			inverseJoinColumns = @JoinColumn(name = "memberOfRoleId")
+	)
+	public Set<UserRole> getGroups() {
+		return groups;
+	}
+	
+	public void setGroups(Set<UserRole> groups) {
+		this.groups = groups;
+	}
+
+}
--- trunk/samples/knowit/examples/identity-management/ejb/src/main/resources/import.sql
+++ trunk/samples/knowit/examples/identity-management/ejb/src/main/resources/import.sql
@@ -1,3 +1,13 @@
+-- SQL statements which are executed at application startup if hibernate.hbm2ddl.auto is 'create' or 'create-drop'
+
+-- admin password is blank
+insert into useraccount    (id, version, username, passwordhash, enabled) values (1, 1, 'admin', 'Ss/jICpf9c9GeJj8WKqx1hUClEE=', true);
+insert into userrole       (id, version, name, conditional) values (1, 1, 'admin', false);
+insert into userrole       (id, version, name, conditional) values (2, 1, 'member', false);
+insert into userrole       (id, version, name, conditional) values (3, 1, 'guest', true);
+insert into useraccountrole(useraccountid, userroleid) values (1, 1);
+insert into userrolegroup  (roleid, memberofroleid) values (1, 2);
+
 insert into Movie (version, director, title, year, plot) values (0, 'Joel Coen', 'The Big Lebowski', 1998, '"Dude" Lebowski, mistaken for a millionaire Lebowski, seeks restitution for his ruined rug and enlists his bowling buddies to help get it.')
 insert into Movie (version, director, title, year, plot) values (0, 'Quentin Tarantino', 'Reservoir Dogs', 1992, 'After a simple jewelery heist goes terribly wrong, the surviving criminals begin to suspect that one of them is a police informant.')
 insert into Movie (version, director, title, year, plot) values (0, 'Joel Coen', 'Fargo', 1996, 'Jerry Lundegaard''s inept crime falls apart due to his and his henchmen''s bungling and the persistent police work of pregnant Marge Gunderson.')
