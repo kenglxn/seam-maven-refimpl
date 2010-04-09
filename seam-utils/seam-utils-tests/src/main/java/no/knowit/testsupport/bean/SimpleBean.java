ed422cd47d2283162a9d738a1f2eddc49cfde6e4
mr.leif.olsen <mr.leif.olsen@1023f760-3e6f-11de-b83a-779c162a6e82>
2010-04-09 20:22:01 +0000

    refactoring seam-utils in progress...

--- seam-utils/seam-utils-tests/src/main/java/no/knowit/testsupport/bean/SimpleBean.java
+++ seam-utils/seam-utils-tests/src/main/java/no/knowit/testsupport/bean/SimpleBean.java
@@ -1,8 +1,23 @@
 package no.knowit.testsupport.bean;
 
 public class SimpleBean {
+  public enum Color {RED, YELLOW, GREEN};
+  
   private Integer id;
   private int foo; 
   protected String bar;
   public String baz;
+  public Color color;
+  
+  private void setFoo(int foo) {
+    this.foo = foo * 2;
+  }
+  
+  protected void setBar(final String bar) {
+    this.bar = "setBar -> " + bar;
+  }
+  
+  public void setBaz(String baz) {
+    this.baz = baz;
+  }
 }
