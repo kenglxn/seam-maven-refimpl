74fb82cd5b9946a570e0147a257ac52276d06522
mr.leif.olsen <mr.leif.olsen@1023f760-3e6f-11de-b83a-779c162a6e82>
2010-05-04 20:55:38 +0000

    example project for jpa talk

--- sandbox/loo/jpa-continued/project/ejb/src/test/java/no/knowit/javabin/model/MovieTest.java
+++ sandbox/loo/jpa-continued/project/ejb/src/test/java/no/knowit/javabin/model/MovieTest.java
@@ -10,6 +10,9 @@ import no.knowit.openejb.mock.OpenEjbTest;
 
 public class MovieTest extends OpenEjbTest {
 
+  private Movie theWall;
+  private Integer fargoId;
+  
   private CrudService lookupCrudService() throws Exception {
     return doJndiLookup(CrudService.NAME);
   }
@@ -36,6 +39,40 @@ public class MovieTest extends OpenEjbTest {
     Movie movie = crudService.persist(new Movie("Alan Parker", "The Wall", 2000));
     Assert.assertNotNull(movie, "crudService.persist: movie was null");
     Assert.assertNotNull(movie.getId(), "movie.getId: movie.id was null");
+    
+    // Keep for read, update and delete tests
+    theWall = movie;
+    movie = crudService.persist(new Movie("Joel Coen", "Fargo", 1996));
+    fargoId = movie.getId();
   }
 
+  @Test(dependsOnMethods={ "create" })
+  public void read() throws Exception {
+    CrudService crudService = lookupCrudService();
+
+    Movie movie = crudService.find(Movie.class, theWall.getId());
+    Assert.assertNotNull(movie, "crudService.find: Did not find movie with id: " + theWall.getId());
+
+    movie = crudService.find(Movie.class, fargoId);
+    Assert.assertNotNull(movie, "crudService.find: Did not find movie with id: " + fargoId);
+  }
+  
+  @Test(dependsOnMethods={ "read" })
+  public void update() throws Exception {
+    CrudService crudService = lookupCrudService();
+    theWall.setYear(1999);
+    theWall = crudService.merge(theWall);
+    Assert.assertEquals(theWall.getYear(), new Integer(1999));
+  }
+  
+  @Test(dependsOnMethods={ "update" })
+  public void delete() throws Exception {
+    CrudService crudService = lookupCrudService();
+    
+    crudService.remove(Movie.class, fargoId);
+    Assert.assertNull(crudService.find(Movie.class, fargoId));
+    
+    crudService.remove(theWall);
+    Assert.assertNull(crudService.find(Movie.class, theWall.getId()));
+  }
 }
