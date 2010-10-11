cddb50b0db06ca207a5203870104e84a5147cc7f
mr.leif.olsen <mr.leif.olsen@1023f760-3e6f-11de-b83a-779c162a6e82>
2010-10-11 23:32:19 +0000

    Refactoring ToStringbuilder - work in progress...

--- trunk/seam-utils/seam-utils-tests/src/main/java/no/knowit/testsupport/bean/BeanWithNestedClasses.java
+++ trunk/seam-utils/seam-utils-tests/src/main/java/no/knowit/testsupport/bean/BeanWithNestedClasses.java
@@ -1,26 +1,36 @@
 package no.knowit.testsupport.bean;
 
 public class BeanWithNestedClasses {
-  class InnerClass {
+  private class InnerClass {
     private int i;
   }
 
-  static class StaticNestedClass {
-    private int i;
-  }
- 
-//  private int i;
+//  private static class StaticNestedClass {
+//    private int i;
+//  }
+// 
+//  public enum NestedEnum {RED, YELLOW, GREEN};
+//  
+//  public interface NestedInterface {
+//  }
+
   private InnerClass innerClass;
-  private StaticNestedClass staticNestedClass;
+  
+////  private StaticNestedClass staticNestedClass;
+//  
+////  private int i;
   
   public BeanWithNestedClasses(int i) {
 //    this.i = i;
-//    innerClass = new InnerClass();
-//    innerClass.i = i + 10;
-//    System.out.println(innerClass.getClass().getSimpleName());
-//    System.out.println(innerClass.getClass().getName());
     
-    staticNestedClass = new StaticNestedClass();
-    staticNestedClass.i = i + 20;
+    innerClass = new InnerClass();
+    innerClass.i = i + 10;
+    
+//    System.out.println(innerClass.getClass().getSimpleName() + " - >" + innerClass.getClass().getName());
+    
+//    staticNestedClass = new StaticNestedClass();
+//    staticNestedClass.i = i + 20;
+//
+//    System.out.println(staticNestedClass.getClass().getSimpleName() + " -> " + staticNestedClass.getClass().getName());
   }
 }
